cebd2ec999cc8682cfea1503496aeac977f2ed4f
Dave Kanter <moatgoat@hotmail.com>
2018-04-26 12:59:35 -0700

    HTML and CSS complete, issues linking to external CSS via Express.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-
-
 var app = require('express')();
 var http = require('http').Server(app);
 
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/styles'))); //allows html file to reference stylesheet "helloworld.css" that is stored in ./css directory
+
+
+
 app.get('/', function(req, res) {
-	res.send('<h1>Dave rules!</h1>');
+	//res.send('<h1>Dave rules!</h1>');
+	res.sendFile(__dirname + '/index.html');
 });
+
+//app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, function() {
 	console.log('*:3000 is alive!');
