c0f0f48fd8639e8753b0f1836d135fbc386a7cd7
Dave Kanter <moatgoat@hotmail.com>
2018-05-03 11:00:45 -0700

    Working chat in basic form.  Homework improvements next

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var publicPath = path.resolve(__dirname);
+var io = require('socket.io')(http);
+
 /* 	The socket.io page is not clear about the need to explicitly declare
 	all of the above. The example puts the CSS in a <style> tag in the
-	HTML header.
+	HTML header.  This form links to an external file.
 */
 
 app.use(express.static(publicPath));
@@ -17,8 +19,27 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-//app.use(express.static(__dirname + '/styles'));
+io.on('connection', function(socket){
+	console.log('Another user connected!');
+
+	//node will let you send 'custom' events using emit
+	socket.on('cmsg', function(msg) {
+		/*	this will send to *everyone*
+			Use 'broadcast' to control who it gets sent to
+		*/
+		
+		//test it:
+		//console.log('message: ' + msg);
+		io.emit('cmsg', msg);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('Another user disconnected!');
+	});
+});
+
 
 http.listen(3000, function() {
 	console.log('*:3000 is alive!');
 });
+
